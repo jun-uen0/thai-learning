1b7e0ec99e4ea95430c5f18b82e0a35194a9878f
Jun Ueno <hello@junueno.dev>
2024-11-16 10:01:27 +0900

    remove unnecessary import

--- src/main/java/com/thai/language/learning/service/ToneQuestionService.java
+++ src/main/java/com/thai/language/learning/service/ToneQuestionService.java
@@ -7,7 +7,6 @@ import com.thai.language.learning.domain.ToneQuestion;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
